f6dfc991462f2d3b4420c35e10dc486b2e713c52
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-12 12:04:26 -0400

    constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,29 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+  end
+  
+  # def brand
+  #   @brand
+  #   BRANDS.uniq
+  # end
+  
+  def brand=(brand)
+    @brand = brand
+    brands = BRANDS.uniq
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 
 end
